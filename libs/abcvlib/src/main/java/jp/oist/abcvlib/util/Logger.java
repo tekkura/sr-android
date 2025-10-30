5f1df997eed77f16c2af93931883be65596beb46
Christopher Buckley <topherbuckley@gmail.com>
2025-10-30 20:29:44 +0900

    Switch debug to DEBUG build variant and fix for missing variants in build

--- libs/abcvlib/src/main/java/jp/oist/abcvlib/util/Logger.java
+++ libs/abcvlib/src/main/java/jp/oist/abcvlib/util/Logger.java
@@ -5,41 +5,41 @@ import jp.oist.abcvlib.core.BuildConfig;
 
 /**
  * Custom logger wrapper.
- * Logs (v/d/i/w) only for debug builds.
+ * Logs (v/d/i/w) only for DEBUG builds.
  * Errors (e) are always logged.
  */
 public class Logger {
 
     public static void v(String tag, String msg) {
-        if (!BuildConfig.optimized) Log.v(tag, msg);
+        if (BuildConfig.DEBUG) Log.v(tag, msg);
     }
 
     public static void v(String tag, String msg, Throwable tr) {
-        if (!BuildConfig.optimized) Log.v(tag, msg, tr);
+        if (BuildConfig.DEBUG) Log.v(tag, msg, tr);
     }
 
     public static void d(String tag, String msg) {
-        if (!BuildConfig.optimized) Log.d(tag, msg);
+        if (BuildConfig.DEBUG) Log.d(tag, msg);
     }
 
     public static void d(String tag, String msg, Throwable tr) {
-        if (!BuildConfig.optimized) Log.d(tag, msg, tr);
+        if (BuildConfig.DEBUG) Log.d(tag, msg, tr);
     }
 
     public static void i(String tag, String msg) {
-        if (!BuildConfig.optimized) Log.i(tag, msg);
+        if (BuildConfig.DEBUG) Log.i(tag, msg);
     }
 
     public static void i(String tag, String msg, Throwable tr) {
-        if (!BuildConfig.optimized) Log.i(tag, msg, tr);
+        if (BuildConfig.DEBUG) Log.i(tag, msg, tr);
     }
 
     public static void w(String tag, String msg) {
-        if (!BuildConfig.optimized) Log.w(tag, msg);
+        if (BuildConfig.DEBUG) Log.w(tag, msg);
     }
 
     public static void w(String tag, String msg, Throwable tr) {
-        if (!BuildConfig.optimized) Log.w(tag, msg, tr);
+        if (BuildConfig.DEBUG) Log.w(tag, msg, tr);
     }
 
     public static void e(String tag, String msg) {
