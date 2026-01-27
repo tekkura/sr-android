380917e3165b29e0d5bf53466cd0952a21ead1fc
ma7moud3ly <engma7moud3ly@gmail.com>
2026-01-27 07:16:21 +0200

    Upgrade Kotlin Gradle plugin from to 1.9.24

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,7 +7,7 @@ buildscript {
     }
     dependencies {
         classpath("com.android.tools.build:gradle:8.13.2")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.6.21") // needed for YuvToRgbConverter
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.24") // needed for YuvToRgbConverter
         classpath("de.undercouch:gradle-download-task:5.3.1")
     }
 }
