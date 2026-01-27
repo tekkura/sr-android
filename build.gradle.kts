80619c429edca4df016b6deb499df12b710588f1
ma7moud3ly <engma7moud3ly@gmail.com>
2026-01-27 06:25:39 +0200

    Update agp (android gradle plugin) to 8.13.2

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,7 @@ buildscript {
         jcenter()
     }
     dependencies {
-        classpath("com.android.tools.build:gradle:8.1.1")
+        classpath("com.android.tools.build:gradle:8.13.2")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.6.21") // needed for YuvToRgbConverter
         classpath("de.undercouch:gradle-download-task:5.3.1")
     }
