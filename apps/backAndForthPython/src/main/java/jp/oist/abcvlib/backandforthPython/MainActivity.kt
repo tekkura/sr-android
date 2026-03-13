4356ab81fd227b911fc63317d34ad06b4f7ffb59
ma7moud3ly <engma7moud3ly@gmail.com>
2026-03-13 05:20:07 +0200

    Implement BackAndForth app using Chaquopy Python

--- apps/backAndForthPython/src/main/java/jp/oist/abcvlib/backandforthPython/MainActivity.kt
+++ apps/backAndForthPython/src/main/java/jp/oist/abcvlib/backandforthPython/MainActivity.kt
@@ -1,8 +1,15 @@
 package jp.oist.abcvlib.backandforthPython
 
 import android.os.Bundle
+import androidx.lifecycle.lifecycleScope
+import com.chaquo.python.PyException
+import com.chaquo.python.Python
+import com.chaquo.python.android.AndroidPlatform
 import jp.oist.abcvlib.core.AbcvlibActivity
+import jp.oist.abcvlib.util.Logger
 import jp.oist.abcvlib.util.UsbSerial
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 
 class MainActivity : AbcvlibActivity() {
@@ -13,5 +20,26 @@ class MainActivity : AbcvlibActivity() {
 
     override fun onSerialReady(usbSerial: UsbSerial) {
         super.onSerialReady(usbSerial)
+        initPython()
+    }
+
+    private fun initPython() {
+        if (!Python.isStarted()) {
+            Python.start(AndroidPlatform(this))
+        }
+
+        val py = Python.getInstance()
+        val setupModule = py.getModule("abcvlib")
+        // inject variables to python
+        setupModule.put("outputs", outputs)
+        setupModule.put("loop_delay", 0.005) // 5 ms
+
+        lifecycleScope.launch(Dispatchers.Default) {
+            try {
+                setupModule.callAttr("run")
+            } catch (e: PyException) {
+                Logger.e("MainActivity", "Python error: ${e.message}")
+            }
+        }
     }
 }
