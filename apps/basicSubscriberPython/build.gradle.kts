20c73d20aa6ff56fbfa9a704ae2ec1f0fd2bc38f
ma7moud3ly <engma7moud3ly@gmail.com>
2026-03-18 00:51:00 +0200

    Clone BasicSubscriber app

---
+++ apps/basicSubscriberPython/build.gradle.kts
@@ -0,0 +1,18 @@
+plugins {
+    alias(libs.plugins.oist.application)
+    alias(libs.plugins.chaquopy)
+}
+
+android {
+    namespace = "jp.oist.abcvlib.basicsubscriberPython"
+
+    buildFeatures {
+        viewBinding = true
+    }
+
+    defaultConfig {
+        ndk {
+            abiFilters += listOf("arm64-v8a", "armeabi-v7a", "x86_64", "x86")
+        }
+    }
+}
