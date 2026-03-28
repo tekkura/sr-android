109065d83d2ca4721ed329b8c3076bdcf1dd57d3
ma7moud3ly <engma7moud3ly@gmail.com>
2026-03-28 20:53:16 +0200

    ros(feat): Implement rosbridge-test app

---
+++ apps/rosbridgeTest/build.gradle.kts
@@ -0,0 +1,15 @@
+plugins {
+    alias(libs.plugins.oist.application)
+}
+
+android {
+    namespace = "jp.oist.abcvlib.rosbridge"
+
+    buildFeatures {
+        viewBinding = true
+    }
+
+    dependencies {
+        implementation(libs.okhttp)
+    }
+}
